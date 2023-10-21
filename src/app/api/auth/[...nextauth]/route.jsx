15517bd0c8195b50cb44d4dcb6f4c6fea09ae3ef
ariztegui8 <jorgeariztegui8@gmail.com>
2023-10-21 00:30:00 -0300

    dashboard credentials

--- src/app/api/auth/[...nextauth]/route.jsx
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from "next-auth/providers/credentials";
+import conectarDB from "@/utils/conectarDB";
+import User from "@/models/User";
+import bcrypt from 'bcryptjs'
 
 const handler = NextAuth({
     providers: [
@@ -11,12 +14,17 @@ const handler = NextAuth({
                 password: { label: "Password", type: "password", placeholder: "Ingrese su password" }
             },
             async authorize(credentials, req) {
-                const user = { id: "1", fullname: "Jorge", email: "jorge08@gmail.com" }
-                if (user) {
-                    return user
-                } else {
-                    return null
-                }
+                await conectarDB()
+                console.log(credentials);
+
+                const userFound = await User.findOne({email: credentials?.email}).select("+password")
+                if(!userFound) throw new Error('Invalid credentials')
+
+                const passwordMatch = await bcrypt.compare(credentials.password, userFound.password)
+                if(!passwordMatch) throw new Error('Invalid credentials')
+
+                console.log(userFound);
+                return userFound
             }
         }),
 
@@ -24,7 +32,17 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-    ]
+    ],
+    callbacks: {
+        jwt({account, token, user, profile, session}){
+            if(user) token.user = user;
+            return token;
+        },
+        session({session, token}){
+           session.user = token.user
+            return session
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,12 @@
+'use client'
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const Dashboard = () => {
+
+  const {data: session, status} = useSession()
+  console.log(session, status);
+
   return (
     <div>Dashboard</div>
   )
